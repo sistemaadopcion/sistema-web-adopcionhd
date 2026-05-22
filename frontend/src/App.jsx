08f0f3b961bfdeefa9331838c8a0ef9dbdc9b462
César Chavez <ceanchavz18@gmail.com>
2026-05-22 00:40:36 -0500

    Implementacion del dashboard y sidebar del admnistrador

---
+++ frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import AdminLayout from "./layouts/AdminLayout";
+
+import AdminDashboard from "./pages/AdminDashboard";
+import Mascotas from "./pages/Mascotas";
+import Solicitudes from "./pages/Solicitudes";
+import Usuarios from "./pages/Usuarios";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        {/* Redirección */}
+        <Route
+          path="/"
+          element={<Navigate to="/admin/dashboard" />}
+        />
+
+        {/* Layout Admin */}
+        <Route path="/admin" element={<AdminLayout />}>
+
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="mascotas" element={<Mascotas />} />
+          <Route path="solicitudes" element={<Solicitudes />} />
+          <Route path="usuarios" element={<Usuarios />} />
+
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Solicitudes.jsx
@@ -0,0 +1,7 @@
+export default function Solicitudes() {
+  return (
+    <div className="p-8 text-3xl font-bold">
+      Página de Solicitudes
+    </div>
+  );
+}
