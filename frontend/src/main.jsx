4727d86e49daf6497286171d674f6dcc9ed207d3
César Chavez <ceanchavz18@gmail.com>
2026-07-10 04:48:02 -0500

    tai

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import './index.css' // <--- AGREGA ESTA LÍNEA AQUÍ
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
